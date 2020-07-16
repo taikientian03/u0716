c36276ce0f4897fb7b2c47278eb138e91edbc4f1
fusatoshi3104 <satoshidat@gmail.com>
2020-07-16 12:23:13 +0900

    Initial commit

---
+++ Assets/hensu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hensu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void  proc()
+    {
+        int a= 10;
+        float fw= 3.14f;
+        string str= "こんにちは";
+        bool bl= true;
+
+        Debug.Log(a);
+        Debug.Log(fw);
+        Debug.Log(str);
+        Debug.Log(bl);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
