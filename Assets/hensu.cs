9b8b06e5d241125c4aa567ef043ab729446cd9c6
fusatoshi3104 <satoshidat@gmail.com>
2020-07-22 16:11:32 +0900

    変数

--- Assets/hensu.cs
+++ Assets/hensu.cs
@@ -4,6 +4,30 @@ using UnityEngine;
 
 public class hensu : MonoBehaviour
 {
+    int vx = 20;
+
+    //順次処理、分岐処理、繰り返し処理
+    public void ensyu71()  
+    {
+        int vx = 5;
+        Debug.Log(vx);
+
+        vx = 10;
+        Debug.Log(vx);
+        Debug.Log(vx);
+    }
+
+    public void ensyu72()
+
+    {
+        Debug.Log(vx);
+    }
+    public void ensyu74()
+    {
+        vx += 10;
+        Debug.Log(vx);
+    }
+
     // Start is called before the first frame update
     public void  proc()
     {
